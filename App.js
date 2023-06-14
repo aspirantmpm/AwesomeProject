85720b9262ab8284a2a23a1cf050071a47851a35
Vadym <aspirantmpm@gmail.com>
2023-06-14 15:50:07 +0300

    start

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { RegisterForm } from './RegistrationScreen';
 
 export default function App() {
+
+const keyboardHide = () => {
+  setIsShowKeyboard(true);
+  Keyboard.dismiss();
+  console.log(state);
+  setState(initialState);
+};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground style={styles.image} source={require('./assets/images/PhotoBG.png')}>
+          {/* <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}> */}
+            <RegisterForm onPress={keyboardHide} />
+            {/* <StatusBar style="auto" /> */}
+          {/* </KeyboardAvoidingView> */}
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
+    // alignContent: 'flex-end',
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  image: {
+    // alignContent: 'flex-end',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
+    // alignItems: 'flex-end',
   },
 });
---
+++ RegistrationScreen.js
@@ -0,0 +1,152 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+} from 'react-native';
+import React, { useState } from 'react';
+
+const initialState = {
+  email: '',
+  password: '',
+};
+
+export const RegisterForm = () => {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(true);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Реєстрація</Text>
+        </View>
+
+        <View style={{ ...styles.form, marginBottom: setIsShowKeyboard ? 32 : 43 }}>
+          <View>
+            <TextInput
+              style={styles.input}
+              textAlign="center"
+              onFocus={() => setIsShowKeyboard(true)}
+              onChangeText={value => setState(prevState => ({ ...prevState, name: value }))}
+              value={state.name}
+            />
+          </View>
+          <View>
+            <TextInput
+              style={styles.input}
+              textAlign="center"
+              onFocus={() => setIsShowKeyboard(true)}
+              onChangeText={value => setState(prevState => ({ ...prevState, email: value }))}
+              value={state.email}
+            />
+          </View>
+          <View>
+            <TextInput
+              style={styles.input}
+              textAlign="center"
+              secureTextEntry={true}
+              onFocus={() => setIsShowKeyboard(true)}
+              onChangeText={value => setState(prevState => ({ ...prevState, password: value }))}
+              value={state.password}
+            />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+
+      <TouchableOpacity activeOpacity={0.6} style={styles.btn}>
+        <Text style={styles.btnTitle}>Зареєструватися</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // alignItems: 'flex-start',
+    //   alignContent: 'flex-end',
+    // flex: 1,
+    backgroundColor: '#fff',
+    // justifyContent: 'flex-end',
+    height: 549,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+  //   image: {
+  //     flex: 1,
+  //     resizeMode: 'cover',
+  //     justifyContent: 'center',
+  //     // alignItems: 'center',
+  //   },
+  form: {
+    // marginTop: 30,
+    marginHorizontal: 16,
+    gap: 16,
+    // marginBottom: 43,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+    backgroundColor: '#f6f6f6',
+    height: 50,
+    borderRadius: 8,
+    color: '#BDBDBD',
+
+    // paddingHorizontal: 20,
+    fontSize: 16,
+  },
+  inputTitle: {
+    color: '#CFFFC2',
+    marginBottom: 10,
+    fontSize: 18,
+  },
+  btn: {
+    height: 50,
+    borderRadius: 100,
+    borderWidth: 1,
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 16,
+    ...Platform.select({
+      ios: {
+        backgroundColor: 'transparent',
+        borderColor: '#FF6C00',
+      },
+      android: {
+        backgroundColor: '#FF6C00',
+        borderColor: 'transparent',
+      },
+    }),
+    // backgroundColor: Platform.OS === 'ios' ? 'transparent' : '#31E000',
+    // borderColor: Platform.OS === 'ios' ? '#31E000' : 'transparent',
+  },
+  btnTitle: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  header: {
+    alignItems: 'center',
+    marginTop: 92,
+    marginBottom: 32,
+  },
+  headerTitle: {
+    // alignItems: 'center',
+    color: '#000',
+    fontSize: 30,
+    // marginLeft: 125,
+    // fontFamily: 'Rancho-Regular',
+  },
+});

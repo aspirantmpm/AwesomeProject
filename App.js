6f5c93af4a245247712c79c6a86b104d46636cb0
Vadym <aspirantmpm@gmail.com>
2023-06-17 12:00:04 +0300

    Debugger

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Keyboard } from 'react-native';
+import { StyleSheet } from 'react-native';
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,14 +10,6 @@ import { Home } from './Screens/HomeScreen';
 const MainStack = createStackNavigator();
 
 export default function App() {
-  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
-  const keyboardHide = () => {
-    setIsShowKeyboard(true);
-    Keyboard.dismiss();
-    console.log(state);
-    setState(initialState);
-  };
-
   return (
     <NavigationContainer style={styles.navigatorContainer}>
       <MainStack.Navigator initialRouteName="LoginScreen">
